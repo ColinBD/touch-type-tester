8851d78c3a7f28e051d8420076b45901c744073c
Colin Bone Dodds <colinbonedodds@gmail.com>
2019-09-02 19:02:47 +0100

    fan position set

--- src/App.js
+++ src/App.js
@@ -23,15 +23,60 @@ class App extends Component {
       quantity: ""
     }
   };
+
+  componentDidMount() {
+    /*
+    CALCULATE AND SET POSITION OF FAN
+    original image:
+      height: 1167px
+      width: 3021px
+      fan pos from centre: 1010px
+      fan pos from top: 248px
+
+    Do: 
+    //1010/1167 * innerHeight = num pixels fan should be from center
+    (innerWidth/2) - (1010/1167 * innerHeight) = fan's left position 
+
+    248/1167 * innerHeight = num pixels fan should be from top
+    */
+
+    //calculate then set the fan's size via width prop
+    console.log("scaler", window.innerHeight);
+    const width = (window.innerHeight / 550) * 200;
+    document.getElementsByClassName("fan")[0].style.width = "" + width + "px";
+
+    //calculate then set fan's left position
+    const offsetLeft = 80;
+    const left =
+      window.innerWidth / 2 - ((1010 - offsetLeft) / 1167) * window.innerHeight;
+    document.getElementsByClassName("fan")[0].style.left = "" + left + "px";
+
+    //calculate then set fan's top position
+    const top = (255 / 1167) * window.innerHeight;
+    document.getElementsByClassName("fan")[0].style.top = "" + top + "px";
+
+    if (window.innerWidth / window.innerHeight > 2.589) {
+      console.log(
+        "full image displayed",
+        window.innerWidth / window.innerHeight
+      );
+    } else {
+      console.log("image cropped", window.innerWidth / window.innerHeight);
+    }
+  }
+
   render() {
     return (
       <React.Fragment>
         <Header />
-        <img src={Fan} alt="fan" width="30%" />
+        <div className="fanWrapper">
+          <img src={Fan} alt="fan" className="fan" />
+        </div>
         {this.checkStage()}
       </React.Fragment>
     );
   }
+
   checkStage() {
     if (this.state.stage === 2) {
       return <Typer settings={this.state.settings} />;
